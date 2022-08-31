902e56f8ead09d79bf1835aa5478bc92c0791251
Levin_Ihor <lytghzys@gmail.com>
2022-08-31 18:07:41 +0300

    npm i and icons component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <VIcons />
+    <VInputForm />
+    <VKeeps />
+  </main>
+</template>
+<script setup>
+import VIcons from "./components/v-icons.vue";
+import VInputForm from "./components/v-input-form.vue";
+import VKeeps from "./components/v-keeps.vue";
+</script>
+
+<style scoped>
+main{
+    padding: 40px;
+}
+
+
+</style>
---
+++ src/components/v-icons.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="icons">
+    <img src="https://keep-vue.netlify.app/img/keep-logo.579de524.png" alt="" />
+    <span>+</span>
+    <img src="https://keep-vue.netlify.app/img/vue-logo.82b9c7a5.png" alt="" />
+    <span>+</span>
+    <img
+      src="https://keep-vue.netlify.app/img/firebase-logo.c24b6b9c.png"
+      alt=""
+    />
+  </div>
+</template>
+<style scoped>
+.icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  max-width: 40px;
+  margin: 0 10px;
+  object-fit: cover;
+}
+img:last-child{
+    max-width: 30px;
+}
+span {
+  margin: 0 10px;
+  font-size: 20px;
+  color: rgb(10, 108, 3);
+}
+</style>
